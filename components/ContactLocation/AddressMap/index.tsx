dc5e13a6cd73d7e7b3d4980130f0475e257f4c3d
naiianable <78005139+naiianable@users.noreply.github.com>
2023-06-11 23:35:56 -1000

    Contact location component filled except map

---
+++ components/ContactLocation/AddressMap/index.tsx
@@ -0,0 +1,23 @@
+const AddressMap = () => {
+  return (
+    <>
+      <div className='w-1/2 mobile:w-full mobile:mt-[36px]'>
+        <div className='Address Div'>
+          <div>Raul T. Sabat, MFT</div>
+          <div>1111 Bishop Street #511</div>
+          <div>Honolulu, HI 96813</div>
+        </div>
+        <div className='Google Map Div mobile:mt-[32px] mobile:mb-[16px]'>
+          Google Map Here
+        </div>
+        <div className=''>
+          Our office is located in the AC Hotel Honolulu building (previously
+          known as the Remington College Building) in the heart of downtown
+          Honolulu at the cross streets of Bishop St. and S. Hotel St.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddressMap;
--- components/ContactLocation/Contact/index.tsx
+++ components/ContactLocation/Contact/index.tsx
@@ -1,8 +1,18 @@
+import ButtonContainer from '@/components/ActionButtons/ButtonContainer';
+
 const Contact = () => {
   return (
     <>
       <div className='w-1/2 mobile:w-full'>
-        Please call or email for appointment 1-808-495-0140
+        <div>Please call or email for appointment 1-808-495-0140</div>
+        {/* Adjust laptop responsiveness */}
+        <div className='my-[16px]'>
+          <ButtonContainer />
+        </div>
+        <div>
+          Raul Sabat can be reached by phone during normal business hours. If
+          you have a medical emergency, please dial 911.
+        </div>
       </div>
     </>
   );
