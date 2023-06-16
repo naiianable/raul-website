74bc3368476e7c604166ab1a19f520985e8f0ae0
naiianable <78005139+naiianable@users.noreply.github.com>
2023-06-16 00:10:07 -1000

    Footer added

---
+++ components/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='flex justify-center items-center h-12 mobile:text-[12px] laptop:text-lg text-primary-white w-full bg-primary-blue'>
+      © 2023 Raul Sabat MFT, LLC. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import LandingPage from '../components/LandingPage';
 import AboutMe from '../components/AboutMe';
 import ServicesProvided from '../components/ServicesProvided';
 import ContactLocation from '@/components/ContactLocation';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutMe />
       <ServicesProvided />
       <ContactLocation />
+      <Footer />
     </div>
   );
 }
